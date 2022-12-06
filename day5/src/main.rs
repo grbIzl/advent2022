864484c4b47dc2911e0b36407e67d2dfbecfe205
Tamas Nemeth <tamas.nemeth@bvtech.io>
2022-12-06 02:06:33 +0100

    removed manual inputs

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -5,46 +5,25 @@ use std::io::{BufRead, BufReader};
 #[macro_use]
 extern crate scan_fmt;
 
-// Sorry :D
-fn prepare_stacks(part: &str) -> Vec<LinkedList<char>> {
-    let mut v = Vec::new();
-    if part == "part1" {
-        v.push(LinkedList::from(['_']));
-        v.push(LinkedList::from(['Z', 'N']));
-        v.push(LinkedList::from(['M', 'C', 'D']));
-        v.push(LinkedList::from(['P']))
-    } else if part == "part2" {
-        v.push(LinkedList::from(['_']));
-        v.push(LinkedList::from(['D', 'L', 'V', 'T', 'M', 'H', 'F']));
-        v.push(LinkedList::from(['H', 'Q', 'G', 'J', 'C', 'T', 'N', 'P']));
-        v.push(LinkedList::from(['R', 'S', 'D', 'M', 'P', 'H']));
-        v.push(LinkedList::from(['L', 'B', 'V', 'F']));
-        v.push(LinkedList::from(['N', 'H', 'G', 'L', 'Q']));
-        v.push(LinkedList::from(['W', 'B', 'D', 'G', 'R', 'M', 'P']));
-        v.push(LinkedList::from(['G', 'M', 'N', 'R', 'C', 'H', 'L', 'Q']));
-        v.push(LinkedList::from(['C', 'L', 'W']));
-        v.push(LinkedList::from(['R', 'D', 'L', 'Q', 'J', 'Z', 'M', 'T']));
-    }
-    v
-}
-
 fn prepare_commands(input: &str) -> (Vec<LinkedList<char>>, Vec<(u8, u8, u8)>) {
-    let mut temp_stacks = Vec::new();
     let file = File::open(input).unwrap();
     let reader = BufReader::new(file);
+
+    let mut temp_stacks = Vec::new();
     let mut stacks = Vec::new();
     let mut commands = Vec::new();
 
     for line in reader.lines() {
-        // Building the stack
         let line = line.unwrap();
         if line.trim().is_empty() || line.trim().starts_with("1") {
             continue;
+        //Commands parsing
         } else if line.trim().starts_with("move") {
             let (count, from, to) = scan_fmt!( &line,  // input string
                             "move {} from {} to {}",     // format
                             u8, u8, u8).unwrap();
             commands.push((count, from, to));
+        //Matrix building of the stacks
         } else {
             let mut fixed = line.replace("    ", "_").replace("[", "").replace("]", "");
             fixed.retain(|c| !c.is_whitespace());
@@ -52,6 +31,7 @@ fn prepare_commands(input: &str) -> (Vec<LinkedList<char>>, Vec<(u8, u8, u8)>) {
             temp_stacks.push(vec)
         }
     }
+    // Building the stack from the matrix
     for i in 0..temp_stacks[0].len() {
         let mut stack = LinkedList::new();
         for j in (0..temp_stacks.len()).rev() {
@@ -80,7 +60,7 @@ fn main() {
         let elem = stack.pop_back().unwrap();
         print!("{elem}")
     }
-    println!("");
+    println!();
     //Part 2
     for (count, from, to) in &commands {
         let mut helper_list = LinkedList::new();
